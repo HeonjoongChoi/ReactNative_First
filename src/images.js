ca7a0012ee157e6db727e0428d51055c7adaa4e9
최헌중 <luna1819@icloud.com>
2021-05-21 10:18:00 +0900

    2nd commit

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import check1 from '../assets/icons/check.png';
+import check2 from '../assets/icons/check2.png';
+import check3 from '../assets/icons/check3.png';
+import check4 from '../assets/icons/check4.png';
+
+export const images = {
+    incomplete: check1,
+    completed: check2,
+    delete: check3,
+    update: check4,
+}
